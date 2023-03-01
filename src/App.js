a53159762da3708ae314c99858ca48decb0069b7
Bernie Lam <bernie@Berniede-MBP.cust.communityfibre.co.uk>
2023-03-01 23:12:30 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
 import axios from "axios";
+import Character from "./components/character";
 
 class App extends Component {
-  state = {};
+  state = { simpsons: [] };
 
   async componentDidMount() {
     const results = await axios.get(
-      `https://thesimpsonsquoteapi.glitch.me/quotes?count=50`
+      `https://thesimpsonsquoteapi.glitch.me/quotes?count=20`
     );
 
     this.setState({ simpsons: results.data });
   }
 
   render() {
-    console.log(this.state);
+    console.log(this.state.simpsons);
+    const { simpsons } = this.state;
 
     //if data is here, loop over the data
     //think defensive check
+    if (!simpsons) {
+      return <h1>Waiting for data....</h1>;
+    }
 
-    return <h1>Waiting for data....</h1>;
+    return (
+      <>
+        {simpsons.map((index) => (
+          <Character index={simpsons} />
+        ))}
+      </>
+    );
   }
 }
 
